e4edaa9ffbaad32fe7de7629ecfe0d5f675fea09
Aleksandra Wierzbiak <a.wierzbiak@wearevirtua.com>
2018-09-27 10:18:22 +0200

    Vue.js firebase config fix

--- applications/turnout/src/firebaseApp.js
+++ applications/turnout/src/firebaseApp.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 const config = {
-    apiKey: "AIzaSyCvpvfheYVR9Djk3aQ4k6pssPciBiBrKx0",
-    authDomain: "turnout-e6778.firebaseapp.com",
-    databaseURL: "https://turnout-e6778.firebaseio.com",
-    projectId: "turnout-e6778",
-    storageBucket: "turnout-e6778.appspot.com",
-    messagingSenderId: "429640650982"
+    apiKey: "your-apikey",
+    authDomain: "your-domain",
+    databaseURL: "your-database-url",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-sender-id"
 };
 
 export const firebaseApp = firebase.initializeApp(config);
